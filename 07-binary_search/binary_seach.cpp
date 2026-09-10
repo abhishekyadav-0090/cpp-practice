8e2cd88eb0b59f7529a8b1a395198356714ee2ee
Abhishek Kumar <abhishekyadavvv0090@gmail.com>
2026-09-10 17:36:38 +0530

    correction to binary search logic

--- 07-binary_search/binary_seach.cpp
+++ 07-binary_search/binary_seach.cpp
@@ -5,21 +5,23 @@ using namespace std;
 int main(){
     vector<int> arr = {10,12,13,24,25,36};
 
-    int target = 25;
+    int target = 27;
     int low = 0;
     //int mid = 0;
     int high = arr.size()-1;
     while(low<=high){
         int mid = (low+high)/2;
-        if(arr[mid]>target){
-            low = mid + 1;
-        }
-        else if(arr[mid]<target){
+        if(arr[mid]>target){          
             high = mid -1;
         }
+        else if(arr[mid]<target){
+            low = mid + 1;
+        }
         else if (arr[mid] == target){
-            cout<<"Element has been found!";
+            cout << "Element has been found!";
+            return 0;
         }
     }
+    cout<<"Element Not Found!";
 
 }
